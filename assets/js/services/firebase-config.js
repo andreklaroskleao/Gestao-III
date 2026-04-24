c60049ef76be49605734f633ae797d7a06d19775
andreklaroskleao <andre.klar.leao@gmail.com>
2026-04-24 11:34:10 -0300

    Update Firebase configuration details

--- assets/js/services/firebase-config.js
+++ assets/js/services/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-aut
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js';
 
 export const firebaseConfig = {
- apiKey: "AIzaSyBfwjjEwhCEe07gSpT-8QGKECBztT7AoUo",
-  authDomain: "gestao-69a63.firebaseapp.com",
-  projectId: "gestao-69a63",
-  storageBucket: "gestao-69a63.firebasestorage.app",
-  messagingSenderId: "821882808210",
-  appId: "1:821882808210:web:5ff38170aa333a3a8e834b"
+ apiKey: "AIzaSyB2p1EEoIqQv1PObCxaXohup4veusFRBwM",
+  authDomain: "gestao-iv.firebaseapp.com",
+  projectId: "gestao-iv",
+  storageBucket: "gestao-iv.firebasestorage.app",
+  messagingSenderId: "964409983695",
+  appId: "1:964409983695:web:51e6ad585198194fda2dba"
 };
 
 export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
